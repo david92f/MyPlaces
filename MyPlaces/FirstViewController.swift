5eac6889b0b7a7559e8c39aa21a08df4451e85f5
user145380 <user145380@87-237-62-7.rdns.melbourne.co.uk>
2018-09-26 19:58:33 +0100

    Initial Commit

---
+++ MyPlaces/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyPlaces
+//
+//  Created by user145380 on 9/26/18.
+//  Copyright © 2018 david_fernandez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyPlaces/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyPlaces
+//
+//  Created by user145380 on 9/26/18.
+//  Copyright © 2018 david_fernandez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
